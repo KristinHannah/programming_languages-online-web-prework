74b7aaea85dc71323f1598e00830f96a2b538014
Kristin Kraemer <kristinhannahk@gmail.com>
2019-05-12 17:05:07 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,7 @@ def reformat_languages(languages)
   languages.each do |styles, lang|
     lang.each do |name, type|
        type.each do |key, value|
-         unless new_hash.include?(name)
+         unless new_hash.include?(name) || =
            new_hash[name] = {key => value, :style => [styles]}
            unless new_hash[name][:style].include?(styles)
               new_hash[name][:style] << styles
