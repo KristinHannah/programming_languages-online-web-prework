983bc5ca7b3242041657b6b894b6ac716769bffc
Kristin Kraemer <kristinhannahk@gmail.com>
2019-05-12 17:29:04 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -8,7 +8,7 @@ def reformat_languages(languages)
          unless new_hash.include?(name) 
            new_hash[name] = {key => value}
            new_hash[name][:style] = [styles]
-           [[styles]] ||= new_hash[name][:style][[:oo, :functional]]
+         #  [[styles]] ||= new_hash[name][:style][[:oo, :functional]]
         end
        end 
        end 
